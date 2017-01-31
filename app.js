844bbb99ffe352443cd9fcb7d3937707d0391599
Abdullah Alansari <ahimta@gmail.com>
2017-01-31 23:38:23 +0300

    Use kue for delayed jobs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const express = require('express');
+const kue = require('kue')
 const webpush = require('web-push');
 
 const bodyParser = require('body-parser');
@@ -11,11 +12,9 @@ const PUBLIC_KEY = 'BBxrA4lbAkt1TiViiJAgeysBQ8Mg7G9URWLDnpe2rfSfFYV26RH7SUWKc0ou
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const app = express();
+const jobQueue = kue.createQueue({ redis: process.env.REDIS_URL });
 
-app.use(cors());
-app.use(bodyParser.json());
-
-app.post('/send-push-msg', (req, res) => {
+jobQueue.process('reminder', (job, done) => {
   const options = {
     vapidDetails: {
       subject: 'https://ahimta.github.io/bagi/',
@@ -25,20 +24,54 @@ app.post('/send-push-msg', (req, res) => {
     TTL: 3600
   };
 
-  const {before, event} = req.body;
-  const data = event ? JSON.stringify(event) : JSON.stringify({});
+  const {before, event: eventString, subscription} = job.data;
+  const event = eventString ? JSON.stringify(eventString) : JSON.stringify({});
+
+  console.log(job);
+
+  webpush.sendNotification(subscription, event, options).then(() => {
+    done();
+  }).catch((err) => {
+    done(new Error(err));
+  });
+})
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(kue.app);
+
+app.post('/send-push-msg', (req, res) => {
+  const {before, event, subscription} = req.body;
+  const millisDifference = event.date - new Date().getTime();
 
   console.log(req.body);
 
-  webpush.sendNotification(req.body.subscription, data, options).then(() => {
-    res.status(200).send({ success: true });
-  }).catch((err) => {
-    if (err.statusCode) {
-      res.status(err.statusCode).send(err.body);
-    } else {
-      res.status(400).send(err.message);
+  if (millisDifference < 0) {
+    res.send(400);
+  } else {
+    let notificationTime;
+    switch (before) {
+      case 'month': notificationTime = event.date - (30 * 24 * 60 * 60 * 1000); break;
+      case 'week': notificationTime = event.date - (7 * 24 * 60 * 60 * 1000); break;
+      case 'day': notificationTime = event.date - (24 * 60 * 60 * 1000); break;
+      case 'hour': notificationTime = event.date - (60 * 60 * 1000); break;
+      case 'minute': notificationTime = event.date - (60 * 1000); break;
+      case 'second': notificationTime = event.date - (1000); break;
+      default: notificationTime = event.date;
     }
-  });
+    const delay = Math.max(new Date(), new Date(notificationTime))
+
+    jobQueue.create('reminder', { before, event, subscription, title: new Date(event.date).toString() })
+      .delay(delay)
+      .save(err => {
+        if (err) {
+          console.log('Couldn\' enqueue reminder: ', req.body);
+          res.send(500, err);
+        } else {
+          res.send(req.body);
+        }
+      });
+  }
 });
 
 const server = app.listen(process.env.PORT || '8080', () => {
